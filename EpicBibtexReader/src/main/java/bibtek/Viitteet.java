35fa66184fced313b31b4e544be6bcac7075aede
Kalle <Kalle@172.17.76.89>
2013-04-11 13:16:41 +0300

    refaktoroinnin aloittelua

---
+++ EpicBibtexReader/src/main/java/bibtek/Viitteet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibtek;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Kalle
+ */
+public class Viitteet {
+    private HashMap<String, HashMap<String, String>> viitteet; // id, viite
+    
+   
+    
+}
