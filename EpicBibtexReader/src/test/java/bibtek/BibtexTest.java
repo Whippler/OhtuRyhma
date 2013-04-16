28f2789757212239b99f0ce9a1462fcd33893abf
Kalle <Kalle@192.168.2.3>
2013-04-16 23:30:40 +0300

    testi fixsattu pois

--- EpicBibtexReader/src/test/java/bibtek/BibtexTest.java
+++ EpicBibtexReader/src/test/java/bibtek/BibtexTest.java
@@ -1,14 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package bibtek;
 
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ *
+ * @author Kalle
+ */
 public class BibtexTest {
-
-    @Test
-    public void hello() {
+    
+    public BibtexTest() {
     }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of createReference method, of class Bibtex.
+     */
+    @Test
+    public void testCreateReference() {
+
+    }
+
+    /**
+     * Test of run method, of class Bibtex.
+     */
+//    @Test
+//    public void testRun() {
+//        System.out.println("run");
+//        Bibtex instance = null;
+//        instance.run();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 }
